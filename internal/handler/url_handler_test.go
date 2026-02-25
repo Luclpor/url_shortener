e82745db2bf7f58fda7cb33faaa57b7a48e25bad
i.konstantionov <i.konstantionov@bi.zone>
2026-02-25 09:40:38 +0300

    iter2 test add handler

---
+++ internal/handler/url_handler_test.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreatedShortURL(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "positive test #1",
+			want: want{
+				code:        http.StatusCreated,
+				contentType: "application/json",
+				response:    `{"short_url":"https://www.google.com"}`,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "https://www.google.com", nil)
+			w := httptest.NewRecorder()
+			CreatedShortURL(w, request)
+			res := w.Result()
+			assert.Equal(t, test.want.code, res.StatusCode)
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			assert.Equal(t, test.want.response, string(resBody))
+			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+		})
+	}
+}
