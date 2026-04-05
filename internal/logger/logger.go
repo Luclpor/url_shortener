e1edcd8a00ec6f43f0396f52ed632472c4aa106b
i.konstantionov <i.konstantionov@bi.zone>
2026-04-05 17:18:12 +0300

    iter12 add log

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -16,9 +18,26 @@ func RequestLogger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		var bodyBytes []byte
+		if r.Body != nil {
+			var err error
+			bodyBytes, err = io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "failed to read request body", http.StatusBadRequest)
+				return
+			}
+
+			r.Body.Close()
+			r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
+		Logger.Info("incoming request",
+			zap.String("method", r.Method),
+			zap.String("url", r.URL.String()),
+			zap.String("body", string(bodyBytes)),
+		)
+
 		h.ServeHTTP(ww, r)
 		Logger.Info(fmt.Sprintf("%s %s %s", r.Method, r.URL, time.Since(start)))
-		Logger.Info(fmt.Sprintf("Request body %s.", r.Body))
 		defer func() {
 			Logger.Info(fmt.Sprintf("request completed response status %d, bytes size %d", ww.Status(), ww.BytesWritten()))
 		}()
