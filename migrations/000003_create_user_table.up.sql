f9b8e881fba32d11536e49dabf278baf58fc3f68
i.konstantionov <i.konstantionov@bi.zone>
2026-04-20 09:30:53 +0300

    iter14 add migrts

---
+++ migrations/000003_create_user_table.up.sql
@@ -0,0 +1,19 @@
+create table if not exists service_user
+(
+    id          uuid default public.uuid_generate_v4() primary key,
+
+    created_at  timestamp with time zone not null,
+                              created_by  varchar(256) not null,
+    updated_at  timestamp with time zone,
+                              updated_by  varchar(256)
+    );
+
+drop trigger if exists tiub_service_user_audit on service_user;
+create trigger tiub_service_user_audit
+    before insert or update
+                         on service_user
+                         for each row
+                         execute procedure audit_table();
+
+alter table url_shortener add column user_id uuid not null;
+alter table url_shortener add  CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES service_user(id) ON DELETE cascade;
