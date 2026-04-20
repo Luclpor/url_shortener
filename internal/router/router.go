d55e2f3bfe1ed03369a3c25a0b44bf610240b80c
i.konstantionov <i.konstantionov@bi.zone>
2026-04-20 12:37:57 +0300

    iter14 router fix

--- internal/router/router.go
+++ internal/router/router.go
@@ -23,7 +23,7 @@ func NewRouter(cfg *config.Config, manager *service.URLManager, healthService *s
 	r.Route("/", func(r chi.Router) {
 		r.Use(customMidlleware.Auth(userAuth))
 		r.Post("/api/shorten", handler.NewCreateShortenUlrJSONHandler())
-		r.Get("/api/user/url", handler.NewGetBatchHandler())
+		r.Get("/api/user/urls", handler.NewGetBatchHandler())
 		r.Post("/", handler.NewCreateHandler())
 		r.Post("/api/shorten/batch", handler.NewCreateBatchShortenHandler())
 		r.Get("/{id}", handler.NewGetterHandler())
