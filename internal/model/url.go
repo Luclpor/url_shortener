cd2c9c1df3c5072aa6fbb27ff657c2700a908315
i.konstantionov <i.konstantionov@bi.zone>
2026-04-20 13:11:56 +0300

    iter14 fix

--- internal/model/url.go
+++ internal/model/url.go
@@ -10,8 +10,8 @@ type ShortenURL struct {
 	ID            uuid.UUID
 	UserID        uuid.UUID
 	CorrelationID *string
-	OriginalURL   string
-	ShortURL      string
+	OriginalURL   string `json:"original_url"`
+	ShortURL      string `json:"short_url"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 }
