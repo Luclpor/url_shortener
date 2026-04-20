9b8b569c96ded21e5086ec7ac308b54076df9300
i.konstantionov <i.konstantionov@bi.zone>
2026-04-20 09:45:14 +0300

    iter14 add migrts

--- migrations/000003_create_user_table.down.sql
+++ migrations/000003_create_user_table.down.sql
@@ -1,1 +1,9 @@
-drop table service_user cascade
+ALTER TABLE url_shortener
+DROP CONSTRAINT IF EXISTS user_id_fkey;
+
+ALTER TABLE url_shortener
+DROP COLUMN IF EXISTS user_id;
+
+DROP TRIGGER IF EXISTS tiub_service_user_audit ON service_user;
+
+DROP TABLE IF EXISTS service_user;
