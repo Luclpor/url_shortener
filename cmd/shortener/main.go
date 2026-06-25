f9ebc33787ef4c3cfa6ccd11f72cb2dac4fa50e4
i.konstantionov <i.konstantionov@bi.zone>
2026-06-25 20:11:00 +0300

    iter 23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/Luclpor/url_shortener.git/internal/router/server"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	printBuildInfo(os.Stdout)
+
 	s, err := server.NewServer()
 	if err != nil {
 		log.Fatal(err)
@@ -14,4 +23,24 @@ func main() {
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printBuildInfo(w io.Writer) {
+	fmt.Fprint(w, formatBuildInfo(buildVersion, buildDate, buildCommit))
+}
+
+func formatBuildInfo(version, date, commit string) string {
+	return fmt.Sprintf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		buildInfoValue(version),
+		buildInfoValue(date),
+		buildInfoValue(commit),
+	)
+}
+
+func buildInfoValue(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
 }
